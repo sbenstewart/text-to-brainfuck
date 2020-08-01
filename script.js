40f87186cd1e2e1626d0a856c2e094c2ada0efc7
S.Ben Stewart <sbenstewart@gmail.com>
2020-08-01 08:40:09 +0530

    var in string error

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $("#bf").keypress(function (event) {
 });
 
 function copy(value) {
-    const pagelink = 'Created using: ${document.location.href}';
+    const pagelink = `Created using: ${document.location.href}`;
     value += ' ' + pagelink;
     var $temp = $("<input>");
     $("body").append($temp);
